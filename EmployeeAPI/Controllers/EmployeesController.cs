1a7125fb64fd37695620cae52c8c381579fedd2f
sam-logs <saumya.prabhakar@bhavnacorp.com>
2022-08-01 15:54:17 +0530

    API for employee portfolio

---
+++ EmployeeAPI/Controllers/EmployeesController.cs
@@ -0,0 +1,32 @@
+﻿using EmployeeAPI.Models;
+using EmployeeAPI.Repositories;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EmployeeAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EmployeesController : Controller
+    {
+        private readonly IEmployeeRepo _employeeContent;
+        public EmployeesController(EmployeeContent employeeContent)
+        {
+            _employeeContent = (IEmployeeRepo)employeeContent;
+        }
+        [HttpGet]
+        public async Task<IEnumerable<Employee>> GetEmployees()
+        {
+            return await _employeeContent.Get();
+        }
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Employee>>GetEmployees(int id)
+        {
+            return await _employeeContent.Get(id);
+        }
+    }
+}
---
+++ EmployeeAPI/Models/EmployeeContent.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EmployeeAPI.Models
+{
+    public class EmployeeContent: DbContext
+    {
+        public EmployeeContent(DbContextOptions<EmployeeContent>options)
+            :base(options)
+        {
+            Database.EnsureCreated();
+        }
+        public DbSet<Employee> Employees { get; set; }
+
+    }
+}
